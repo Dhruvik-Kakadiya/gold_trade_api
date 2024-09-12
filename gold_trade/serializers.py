da6ab5f948bb6ae52f70e617ffe72d95c66b958d
dhruvik-aubergine <dhruvik@auberginesolutions.com>
2024-09-13 00:41:45 +0530

    Add Register and Login APIs

---
+++ gold_trade/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """UserSerializer for create user"""
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        # Create user
+        user = User.objects.create_user(**validated_data)
+        return user
--- gold_trade/views.py
+++ gold_trade/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        """API to Register user with username, email and password"""
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully!"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        """API to login into the system with username and password"""
+
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        try:
+            # Fetch user object if exist
+            user = User.objects.get(username=username)
+
+            # Check password
+            if not user.check_password(password):
+                raise ValueError('Incorrect password')
+
+            # Create JWT tokens
+            refresh = RefreshToken.for_user(user)
+
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_200_OK)
+
+        except User.DoesNotExist:
+            return Response({'error': 'Invalid username/password'}, status=status.HTTP_401_UNAUTHORIZED)
+        except ValueError as e:
+            return Response({'error': str(e)}, status=status.HTTP_401_UNAUTHORIZED)
